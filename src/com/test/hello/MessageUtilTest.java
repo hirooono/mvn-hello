78b1d88b1c14e1b05b2ed09cd1a18ee40a1cb368
ono.hirosato <ono.hirosato@SX0000034547-00.g01.fujitsu.local>
2016-10-06 10:57:13 +0900

    updated 10:57

--- src/com/test/hello/MessageUtilTest.java
+++ src/com/test/hello/MessageUtilTest.java
@@ -1,18 +1,16 @@
 package com.test.hello;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import static junit.framework.Assert.*;
 
 import com.main.hello.MessageUtil;
-
 public class MessageUtilTest {
-	@Test
+
 	public void createMessageTest() {
 	// setup
 	MessageUtil messageUtil = new MessageUtil();
 	// when
 	String message = messageUtil.createMessage("Shoichi");
+
 	// then
 	assertEquals("hello, Shoichi", message);
 }
